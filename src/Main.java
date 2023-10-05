de3fef43a449122f3907c810d8314069b04db8d5
WAYKE4 <gitta.tacrhi@mail.ru>
2023-10-05 20:14:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number");
+        int num = scanner.nextInt();
+
+
+    }
+}
+
+
+
+/*
+  cin >> a ;
+  Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number");
+        int num = scanner.nextInt();
+
+        String mou = switch (num) {
+            case 1,2 -> "Январь";
+            case 3,4 -> "Февраль";
+            case 5,6-> "Апрель";
+
+            default -> "Нету такого месяца";
+
+        };
+        System.out.println(mou);
+ */
+
